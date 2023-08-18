584c6c9a2d0fb92326ef3c73fd5476b6fe37ea19
Abdullah Murtaza <99031293+abdiopp@users.noreply.github.com>
2023-08-18 19:38:35 +0500

    non responsive

--- src/pages/Home/Section2.js
+++ src/pages/Home/Section2.js
@@ -289,6 +289,104 @@ export default function Section2() {
           className=" hidden md:flex flex-nowrap"
         />
       </div>
+
+      <div className="flex flex-row gap-5 items-start pb-[177px] ml-[70px] mr-[280px]">
+        <div className="flex flex-col w-4 shrink-0 items-start mt-px mr-px">
+          <div className="text-xs font-['Azo_Sans'] font-bold leading-[34px] text-[#22a75d]">
+            01
+          </div>
+          <div className="text-xs font-['Azo_Sans'] font-bold leading-[34px] text-[#22a75d]">
+            02
+          </div>
+        </div>
+        <div className="opacity-60 freeText leading-[34px] text-white mr-3 w-1/5">
+          Team set-up
+          <br />
+          Launch of informational website
+        </div>
+        <div className="flex flex-col mt-px w-4 shrink-0 items-start">
+          <div className="text-xs font-['Azo_Sans'] font-bold leading-[34px] text-[#2278d4]">
+            01
+          </div>
+          <div className="text-xs font-['Azo_Sans'] font-bold leading-[34px] text-[#2278d4]">
+            02
+          </div>
+          <div className="text-xs font-['Azo_Sans'] font-bold leading-[34px] text-[#2278d4]">
+            03
+          </div>
+          <div className="text-xs font-['Azo_Sans'] font-bold leading-[34px] text-[#2278d4]">
+            04
+          </div>
+          <div className="text-xs font-['Azo_Sans'] font-bold leading-[34px] text-[#2278d4]">
+            05
+          </div>
+        </div>
+        <div className="opacity-60 freeText leading-[34px] text-white mr-1">
+          Presale and exchange listings
+          <br />
+          Blockchain development and launch
+          <br />
+          Launch of our MVP
+          <br />
+          Influencer agency partnerships
+          <br />
+          Marketing and promotion{" "}
+        </div>
+        <div className="flex flex-col justify-between gap-5 w-4 shrink-0 items-start mt-px mr-px">
+          <div className="text-xs font-['Azo_Sans'] font-bold leading-[44px] text-[#ffd100]">
+            01
+          </div>
+          <div className="text-xs font-['Azo_Sans'] font-bold leading-[44px] text-[#ffd100]">
+            02
+          </div>
+          <div className="text-xs font-['Azo_Sans'] font-bold leading-[44px] text-[#ffd100]">
+            03
+          </div>
+        </div>
+        <div className="opacity-60 freeText leading-[34px] text-white mr-10 w-1/5">
+          Celebrity, Creator and Influencer{" "}
+          <div className="w-[303px] h-[142px] flex flex-col items-start">
+            <div className="opacity-60 freeText leading-[20px] text-white contents absolute top-0 left-0 h-[142px] w-full">
+              partnerships
+              <br />
+            </div>
+            <div className="opacity-60 freeText leading-[34px] text-white contents relative w-full">
+              Expansion of creator communities <br /> on our platform <br />{" "}
+              More Marketing and promotion
+            </div>
+          </div>
+          <div className="w-[303px] h-[142px] flex flex-col items-start">
+            <div className="opacity-60 freeText leading-[20px] text-white contents absolute top-0 left-0 h-[142px] w-full">
+              <br />
+            </div>
+            {/* <div className="opacity-60 freeText  text-white contents relative w-full">
+            </div> */}
+          </div>
+        </div>
+        <div className="flex flex-col mt-px w-4 shrink-0 items-start">
+          <div className="text-xs font-['Azo_Sans'] font-bold leading-[34px] text-[#e20613]">
+            01
+          </div>
+          <div className="text-xs font-['Azo_Sans'] font-bold leading-[34px] text-[#e20613]">
+            02
+          </div>
+          <div className="text-xs font-['Azo_Sans'] font-bold leading-[34px] text-[#e20613]">
+            03
+          </div>
+          <div className="text-xs font-['Azo_Sans'] font-bold leading-[34px] text-[#e20613]">
+            04
+          </div>
+        </div>
+        <div className="opacity-60 freeText leading-[34px] text-white">
+          Mainstream partnerships
+          <br />
+          500 active creator communities
+          <br />
+          2Million active community members
+          <br />
+          More Marketing and promotion
+        </div>
+      </div>
     </>
   );
 }
